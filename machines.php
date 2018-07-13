be4ae148e9040cca0d61cfbf05785650f2440042
Aingty <aingty@sorin11.campus.ad.csulb.edu>
2018-07-13 13:51:46 -0700

    Add 3rd Drive for Sorin9

--- machines.php
+++ machines.php
@@ -751,7 +751,7 @@
                                 echo "</br><h4 class=\"handle\" name=".$filename.">Hard Drive #2 [/dev/sdb] </h4>";
                             }
                             //This special case is for sorin12 (Dennis as of 11/13/16) -> 3HDD
-                            if($j==3 and $i==12)
+                            if($j==3 and ($i==12 or $i==9))
                             {
                                 $filename = "smartctl/sorin".$i."-sdc.txt";
                                 echo "</br><h4 class=\"handle\" name=".$filename.">Hard Drive #3 [/dev/sdc] </h4>";
