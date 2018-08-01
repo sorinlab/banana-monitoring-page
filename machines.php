59283fd6bbf595818461d9ade9a0bef6d0ffaf79
a-ridley <abixcastro@gmail.com>
2018-08-01 16:05:23 -0700

    bolding uptime output

--- machines.php
+++ machines.php
@@ -1087,7 +1087,7 @@
 
                                     if(strpos($line,"minute")!==FALSE || strpos($line,"hour")!==FALSE)
                                     {
-                                        echo "<br><strong><font color='green'> UPTIME: ".$line."</strong></font><br>";
+                                        echo "<br><font color='green'><B> UPTIME: ".$line."</b></font><br>";
                                     }
 
 
