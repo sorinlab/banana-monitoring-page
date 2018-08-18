20420dac831a09aa379fa00684e47ad65d26bbe7
Aingty Eung <aingtyeung@yahoo.com>
2018-08-17 17:22:25 -0700

    Formatt Fixes

--- machines.php
+++ machines.php
@@ -539,7 +539,7 @@
                         echo "</ul>"; //End of machine Info
                     }//End of If smartEnabledDisks
                     
-                    echo "<p style=\"text-align: center\">";
+                    echo "<div>";
                     #Grabbing the essential information of each server
                     if($informationSwitch == 1)
                     {
@@ -568,7 +568,7 @@
                             }
                         }
                     }
-                    echo "</p>";
+                    echo "</div>";
                     echo "</div>"; //End of div
                 }//End of Loop
                 //Starting Loop for displaying all of the machines onto the machine monitoring page in banana************
