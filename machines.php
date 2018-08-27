64aebb0d0d045655b67c2c6a87de5880d42cc07d
a-ridley <abixcastro@gmail.com>
2018-08-27 16:18:24 -0700

    removing users from being displayed because it broke the page

--- machines.php
+++ machines.php
@@ -579,28 +579,7 @@
                     $offline_desktops = array(2,5);
                     
                     for($i=1;$i<=13;$i++)
-                    {
-                                            
-                        //Grabbing User information
-                        $informationFilename = "sorin".$i."_information.txt";
-                        if(file_exists($informationFilename))
-                        {   
-                            echo "<br>";
-                            echo "<h4>Users</h4>";
-
-                            $fh=file($informationFilename,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-                            //Process each line in the file
-                            foreach($fh as $line)
-                            {
-
-                                if(strpos($line,"Users:")!==FALSE)
-                                {
-                                    
-                                    echo "<li>".$line."</li>";
-                                    continue;
-                                }     
-                            }   
-                        
+                    {                        
                         //Generate the file name
                         $filename = "sorin".$i.".txt";
                         //Create the div for each machine
