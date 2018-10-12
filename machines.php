559aea451a5926743247ed64a6f96687ef630e6f
a-ridley <abixcastro@gmail.com>
2018-10-11 17:48:30 -0700

    bug fix

--- machines.php
+++ machines.php
@@ -607,7 +607,7 @@
                                 if(strpos($line,"Current")!==FALSE)
                                 {
                                     
-                                    $replacing = ["Current", "users:", "server"];
+                                    $replacing = ["Current", "users:", "server,"];
                                     $replace_with = ["", "", ""];
                                     $new_line = str_replace($replacing, $replace_with, $line);
                                     //$new_line = str_replace('Current', '', $line);
