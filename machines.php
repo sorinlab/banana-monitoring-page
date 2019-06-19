cd2ba5a9da63cffe38ebd61c1a9b88cb73ac3110
Justin Mabutas <justin.troy.mabutas@gmail.com>
2019-06-19 15:08:42 -0700

    Updated Banana Data Raid section
    Banana Data RAID now displays info from sdc and sdd

--- machines.php
+++ machines.php
@@ -147,13 +147,17 @@
                     if (strpos($name,"banana-OS") !== FALSE) 
                     {
                         echo "<h3 class=\"machineName\">banana OS RAID</h3>";
+                        $smartfile1 = "smartctl/banana-sda.txt"; $smartdisk1 = "/dev/sda";
+                        $smartfile2 = "smartctl/banana-sdb.txt"; $smartdisk2 = "/dev/sdb";
+                        $smartEnabledDisks = 2;
                         $MNAME = "banana";
+                        $informationSwitch = 1;
                     }
                     if (strpos($name,"banana-Data") !== FALSE) 
                     {
                         echo "<h3 class=\"machineName\">banana Data RAID</h3>";
-                        $smartfile1 = "smartctl/banana-sda.txt"; $smartdisk1 = "/dev/sda";
-                        $smartfile2 = "smartctl/banana-sdb.txt"; $smartdisk2 = "/dev/sdb";
+                        $smartfile1 = "smartctl/banana-sdc.txt"; $smartdisk1 = "/dev/sdc";
+                        $smartfile2 = "smartctl/banana-sdd.txt"; $smartdisk2 = "/dev/sdd";
                         $smartEnabledDisks = 2;
                         $MNAME = "banana";
                         $informationSwitch = 1;
